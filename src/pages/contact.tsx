12c40a7ff63e5e7f49bf8a4f077fdcb055666c39
Lars Vermeulen <Lars@MacBook-Air-van-Bente.local>
2023-03-14 13:29:43 +0100

    release

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -38,45 +38,91 @@ const contact = () => {
     <div>
           <Header />
       <Sidebar />
-       
-         
-      <h1>Contact Us</h1>
-      <form  onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            id="name"
-            type="text"
-            name="name"
-            value={formValues.name}
-            onChange={handleChange}
-            required
-          />
+      <div className="container my-24 px-6 mx-auto">
+
+  <section className="mb-32 text-center text-gray-800">
+    <div className="max-w-[700px] mx-auto px-3 lg:px-6">
+      <h2 className="text-3xl font-bold mb-12 text-white">Contact us</h2>
+      <form>
+        <div className="form-group mb-6">
+          <input type="text" className="form-control block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
+            placeholder="Name"/>
         </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            value={formValues.email}
-            onChange={handleChange}
-            required
-          />
+        <div className="form-group mb-6">
+          <input type="email" className="form-control block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
+            placeholder="Email address"/>
         </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            name="message"
-            value={formValues.message}
-            onChange={handleChange}
-            required
-          />
+        <div className="form-group mb-6">
+          <textarea className="
+            form-control
+            block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+          " id="exampleFormControlTextarea13"  placeholder="Message"></textarea>
         </div>
-        <button type="submit">Submit</button>
+        <div className="form-group form-check text-center mb-6">
+
+     
+        </div>
+        <button type="submit" className=" w-full
+          px-6
+          py-2.5
+          bg-gradient-to-r from-[#ca2765] to-[#ec5a68]          text-white
+          font-medium
+          text-xs
+          leading-tight
+          uppercase
+          rounded
+          shadow-md
+          hover:bg-blue-700 hover:shadow-lg
+          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-blue-800 active:shadow-lg
+          transition
+          duration-150
+          ease-in-out">Send</button>
       </form>
     </div>
+  </section>
+
+</div>
+</div>
   );
 };
 
